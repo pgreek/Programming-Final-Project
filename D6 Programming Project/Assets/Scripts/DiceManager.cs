f9b1ce6457e5a6c3a76da1ddbfff2afcc5da7fdf
ReversityVII <127150723+ReversityVII@users.noreply.github.com>
2023-04-13 18:28:40 -0400

    Dice Game Logic
    Added most of the game logic: Picking a dice, rules for dice picking, and discarding a dice. Also added generating new dice in old spaces, and dice moving down to the bottom row like they do in the real game

--- D6 Programming Project/Assets/Scripts/DiceManager.cs
+++ D6 Programming Project/Assets/Scripts/DiceManager.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class DiceManager : MonoBehaviour
@@ -8,21 +11,107 @@ public class DiceManager : MonoBehaviour
     public Transform[] Placements;
     public GameObject DicePrefab;
 
+    public int number; //stats generated for a dice at startup
+    public int color;
+
+    //dice statistics for the existing dice in the stack
+    public int CurrentPlacement;
+    public int CurrentNumber; 
+    public int CurrentColor;
+
+    //dice statistics for the dice that the player just chose
+    public int ChosenNumber;
+    public int ChosenColor;
+    public int ChosenPlacement;
+
+    public bool moveOK = false;
+    public bool slotOpen = false;
+
+    public Transform t;
+
     void Start()
     {
-        foreach (Transform t in Placements) //for every transform value in the array, run this code once (which spawns one dice)
-        {
-            int number = Random.Range(0, DiceSprites.Length); //random number 1-6 (0-5)
-            int color = Random.Range(0, 5); //random color 0-5
-            GameObject go = Instantiate(DicePrefab);
-            DicePrefab.GetComponent<Die>().SetValues(number, color); //stores relevant data in the dice's prefab
+        CurrentNumber = -1; //default values for checking if space is free
+        CurrentColor = -1;
 
-            go.GetComponent<SpriteRenderer>().sprite = DiceSprites[number]; //looks through and finds prefab spriterenderer, then changes the sprite to a random sprite it generated earlier
-            go.transform.parent = t; //sets the game object as a child to the current placement transform
-            go.transform.localPosition = Vector3.zero;
+        foreach (Transform t in Placements) //for every transform value in the array, run this code once (which spawns one dice per slot)
+        {
+            DiceGen(t);
         }
 
     }
 
+    void DiceGen(Transform t2) //accepts a transform position
+    {
+        CurrentPlacement = System.Array.IndexOf(Placements, t2); //specific placement for this dice
 
+        number = Random.Range(0, DiceSprites.Length); //random number 1-6 (0-5 in array)
+        color = Random.Range(0, 6); //random color 0-5
+        GameObject go = Instantiate(DicePrefab);
+
+        go.GetComponent<SpriteRenderer>().sprite = DiceSprites[number]; //looks through and finds prefab spriterenderer, then changes the sprite to a random sprite it generated earlier
+        go.transform.parent = t2; //sets the game object as a child to the current placement transform
+        go.transform.localPosition = Vector3.zero;
+    }
+
+    void DiceCheck(int[] chosenDiceStats)
+    {
+        ChosenNumber = chosenDiceStats[0];
+        ChosenColor = chosenDiceStats[1];
+        ChosenPlacement = chosenDiceStats[2]; //accept data sent from clicked dice
+
+        if (CurrentNumber != -1)
+        {
+            if(ChosenNumber == CurrentNumber + 1)
+            {
+                //increase streak
+                moveOK = true;
+                CurrentNumber = ChosenNumber; //the chosen dice inherits the space
+                CurrentColor = ChosenColor;
+
+                Transform t = Placements[chosenDiceStats[2] - 5];
+                DiceGen(t);
+
+            }
+            else if (ChosenColor == CurrentColor)
+            {
+                //increase streak
+                moveOK = true;
+                CurrentNumber = ChosenNumber; //the chosen dice inherits the space
+                CurrentColor = ChosenColor;
+
+                Transform t = Placements[chosenDiceStats[2] - 5];
+                DiceGen(t);
+
+
+            }
+            else if (ChosenNumber == 0 && CurrentNumber == 5)
+            {
+                //increase streak
+                moveOK = true;
+                CurrentNumber = ChosenNumber; //the chosen dice inherits the space
+                CurrentColor = ChosenColor;
+
+                Transform t = Placements[chosenDiceStats[2] - 5];
+                DiceGen(t);
+
+            }
+            else //illegal move
+            {
+                moveOK = false;
+            }
+        }
+        else //space is free, put whatever dice was chosen in here
+        {
+            
+            CurrentNumber = ChosenNumber; //the chosen dice inherits the space
+            CurrentColor = ChosenColor;
+
+
+            //move new dice over old dice
+            moveOK = true;
+            Transform t = Placements[chosenDiceStats[2] - 5];
+            DiceGen(t);
+        } 
+    }
 }
--- D6 Programming Project/Assets/Scripts/Die.cs
+++ D6 Programming Project/Assets/Scripts/Die.cs
@@ -1,19 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Die : MonoBehaviour
 {
+    public DiceManager diceManager; //dicemanager script
+    public Transform currentDiceSlot; //slot where stacking happens
     public SpriteRenderer dieSprite;
     public Color32[] basic; //array list of 5 colors
 
-    public void SetValues(int n, int c)
+    [SerializeField] private int DiceNumber; //number and color for later reference
+    [SerializeField] private int DiceColor;
+    [SerializeField] private int DicePlacement;
+
+    public bool inCurrentSlot = false;
+    public int layerMask = 1 << 5; //bitshift for layer 5, will ignore sprites
+    
+
+    RaycastHit2D hit;
+
+    public void Awake()
     {
-        int number = n; //number and color for later reference
-        int color = c;
+         diceManager = FindObjectOfType<DiceManager>();
+         currentDiceSlot = GameObject.Find("CurrentDiceSlot").transform;
 
-        dieSprite.color = basic[color]; //the color of the dice
+         DiceNumber = diceManager.number;
+         DiceColor = diceManager.color;
+         DicePlacement = diceManager.CurrentPlacement;
 
-        print(n + " " + c);
+         dieSprite.color = basic[DiceColor]; //the color of the dice
+
+        
+    }
+
+    public void OnMouseDown()
+    {
+
+        if (DicePlacement >= 5 && inCurrentSlot == false) //if the dice is on the bottom row and not in the stack
+        {
+
+            int[] temp = new int[3]; //array to send data to diceManager
+
+            temp[0] = DiceNumber;
+            temp[1] = DiceColor;
+            temp[2] = DicePlacement;
+
+            diceManager.SendMessage("DiceCheck", temp); //send dice stats to method within diceManager to check if the move is valid
+
+            
+            if (diceManager.moveOK) //if diceManager has given the OK to move
+            {
+                transform.position = currentDiceSlot.position; //move dice to the stack
+                hit = Physics2D.Raycast(transform.position, Vector2.zero, Mathf.Infinity, 5); 
+
+                if (hit != false) //the raycast hit an object
+                {
+                    Destroy(hit.transform.gameObject); //destroy that object
+                }
+
+                diceManager.slotOpen = true; //this dice's slot is now open
+                inCurrentSlot = true; //dice now occupies current slot
+            }
+
+            diceManager.moveOK = false; 
+        }
+        else if(inCurrentSlot == true) //discard the dice from the stack by clicking on it
+        {
+            print("reached");
+            diceManager.CurrentNumber = -1; //reset values to default
+            diceManager.CurrentColor = -1;
+            Destroy(transform.gameObject);
+        }
+    }
+
+    private void Update() //try and keep this as clean as possible, we don't want too much stuff running every frame for 11 different objects
+    {
+        if (Input.GetMouseButtonDown(0) == true && diceManager.slotOpen == true && DicePlacement == (diceManager.ChosenPlacement - 5)) //a click happened, and a movement was just permitted, and this dice is behind the chosen dice
+        {
+            transform.position = diceManager.Placements[DicePlacement + 5].position;
+            DicePlacement = DicePlacement + 5;
+            diceManager.slotOpen = false;
+        }
     }
 }
